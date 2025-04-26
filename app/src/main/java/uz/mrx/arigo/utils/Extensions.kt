775ace564f38dcd3cc4a3c4d17a8329b1b937b9e
Ismoil-Developer <ismoilkhan555@gmail.com>
2025-04-26 14:40:34 +0500

    probaga

--- app/src/main/java/uz/mrx/arigo/utils/Extensions.kt
+++ app/src/main/java/uz/mrx/arigo/utils/Extensions.kt
@@ -9,3 +9,4 @@ fun <T> flow() = MutableSharedFlow<T>(replay = 1, onBufferOverflow = BufferOverf
 
 fun Fragment.toast(it: String) =
     Toast.makeText(this.requireContext(), it, Toast.LENGTH_SHORT).show()
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
